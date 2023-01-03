ead7d5996ac3468fcc09002c3c5147c022f17807
chron333e <christopher_mascardo@yahoo.com>
2023-01-03 11:52:59 -0600

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+// @Route('/contact')
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>This is the contact page.</p>
+    </div>
+  );
+};
+
+export default Contact;
+
+
